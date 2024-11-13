a86b6d2283b453bbcd76c111328dba14f2305ae1
Eddie Cortes <eccortes@csu.fullerton.edu>
2024-11-12 22:37:33 -0800

    Add PR function

--- oss_stats/github.py
+++ oss_stats/github.py
@@ -14,3 +14,14 @@ def fetch_repositories():
 
 # TODO: Create functions that fetch issues, PRs, commits, number of contributors,
 # and more from acmcsufoss org and EthanThatOneKid/acmcsuf.com using the PyGithub library.
+def fetch_prs():
+    repos = github.get_organization(org).get_repos()
+    num_PRs = 0
+    # Loops through each repo and counts PRs
+    for repo in repos:
+        i = 0
+        while repo.get_pull(i) != None:
+            num_PRs += 1
+            i += 1
+    return num_PRs
+
